6a51b1517c3267f7579de94f609e2f7454cf48bd
rushia <jonasg.13452@gmail.com>
2022-01-23 17:15:31 -0500

    Update Agent.java

--- Scripts/Agent.java
+++ Scripts/Agent.java
@@ -12,10 +12,19 @@ class Agent {
     public int getx() {
         return this.x;
     }
+    public void setx(int n) {
+        this.x = n;
+    }
     public int gety() {
         return this.y;
     }
+    public void sety(int n) {
+        this.y = n;
+    }
     public float getang() {
         return this.ang;
+    }
+    public void setang(float n) {
+        this.ang = n;
     }
 }
