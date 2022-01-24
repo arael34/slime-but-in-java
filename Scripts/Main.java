3cd044aced6b84a9e7a2564759b84fa3c0cf50d6
rushia <jonasg.13452@gmail.com>
2022-01-24 18:32:51 -0500

    Azalea

--- Scripts/Main.java
+++ Scripts/Main.java
@@ -20,18 +20,18 @@ class Main {
         POINT
     }
     final static int FPS = 60;
-    final static int AGENT_COUNT = 500;
+    final static int AGENT_COUNT = 1000;
     final static int AGENT_SPEED = 1;
     final static int AGENT_SIZE = 1;
-    final static Color AGENT_COLOR = Color.PINK;
+    final static Color AGENT_COLOR = new Color(255, 67, 164);
     final static double TURN_SPEED = Math.PI / 8;
     final static int TRAIL_FADE = 50;
     final static int TRAIL_LENGTH = 200;
     final static int WIDTH = 500;
     final static int HEIGHT = 500;
-    final static int SENSOR_SIZE = 3; 
-    final static int SENSOR_DST = 4;
+    final static int SENSOR_SIZE = 2; 
+    final static int SENSOR_DST = 3;
     final static double SENSOR_OFFSET = 0.3;
-    final static Spawn SPAWN_MODE = Spawn.POINT;
-    final static Color BACKGROUND = Color.BLACK;
+    final static Spawn SPAWN_MODE = Spawn.CIRCLE_IN;
+    final static Color BACKGROUND = new Color(150, 0, 0);
 }
