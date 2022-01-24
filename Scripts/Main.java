6a17daecf640ec58f91b644a2830df1a6b3866ce
rushia <jonasg.13452@gmail.com>
2022-01-24 13:17:29 -0500

    Update Main.java

--- Scripts/Main.java
+++ Scripts/Main.java
@@ -20,7 +20,7 @@ class Settings {
         DONUT,
         WATERFALL
     }
-    final static int FPS = 144;
+    final static int FPS = 60;
     final static int AGENT_COUNT = 2000;
     final static int AGENT_SPEED = 1;
     final static int AGENT_SIZE = 1;
@@ -28,11 +28,11 @@ class Settings {
     final static double TURN_SPEED = Math.PI / 6;
     final static int TRAIL_FADE = 50;
     final static int TRAIL_LENGTH = 200;
-    final static int WIDTH = 400;
-    final static int HEIGHT = 400;
+    final static int WIDTH = 200;
+    final static int HEIGHT = 200;
     final static int SENSOR_SIZE = 3; 
     final static int SENSOR_DST = 4;
     final static double SENSOR_OFFSET = 0.3;
-    final static Spawn SPAWN_MODE = Spawn.CIRCLE_IN;
+    final static Spawn SPAWN_MODE = Spawn.WATERFALL;
     final static Color BACKGROUND = Color.BLACK;
 }
