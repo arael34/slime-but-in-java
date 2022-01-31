0a41a50b38b19607de236b7b18d3f6e38b5e1404
rushia <jonasg.13452@gmail.com>
2022-01-31 17:10:55 -0500

    cool settings

--- Scripts/Main.java
+++ Scripts/Main.java
@@ -24,19 +24,19 @@ class Main {
         ROTATE
     }
     final static int FPS = 144;
-    final static int AGENT_COUNT = 500;
-    final static int AGENT_SPEED = 1;
+    final static int AGENT_COUNT = 2000;
+    final static int AGENT_SPEED = 2;
     final static int AGENT_SIZE = 1;
     final static Color AGENT_COLOR = Color.PINK;
     final static double TURN_SPEED = Math.PI / 8;
     final static int TRAIL_FADE = 50;
     final static int TRAIL_LENGTH = 200;
-    final static int WIDTH = 500;
-    final static int HEIGHT = 500;
+    final static int WIDTH = 1000;
+    final static int HEIGHT = 1000;
     final static int SENSOR_SIZE = 1; 
     final static int SENSOR_DST = 3;
     final static double SENSOR_OFFSET = 0.3;
-    final static Spawn SPAWN_MODE = Spawn.WATERFALL;
+    final static Spawn SPAWN_MODE = Spawn.CIRCLE_OUT;
     final static Color BACKGROUND = Color.BLACK;
     final static EdgeType EDGE = EdgeType.BOUNCE;
 }
